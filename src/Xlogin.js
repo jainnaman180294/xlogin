b954083e1798b3870c49b4ce8335af1311074e19
jainnaman180294 <jain.naman180294@gmail.com>
2024-06-02 20:56:48 +0530

    first commit

---
+++ src/Xlogin.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+function Xlogin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (username === "user" && password === "password") {
+      setMessage(`Welcome, ${username}!`);
+    } else {
+      setError("invalid Username & Password");
+    }
+  };
+  return (
+    <div>
+      <h1>Login page</h1>
+      {message ? (
+        <p>{message}</p>
+      ) : (
+        <div>
+          <p>{error}</p>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Username: </label>
+              <input
+                type="text"
+                placeholder="username"
+                onChange={handleUsername}
+                required
+              />
+            </div>
+            <div>
+              <label>Password: </label>
+              <input
+                type="password"
+                placeholder="password"
+                onChange={handlePassword}
+                required
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Xlogin;
